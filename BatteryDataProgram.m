f08d2dabc95c85cb98df528a9996192e16a25c3e
LeoWGordon <53127431+LeoWGordon@users.noreply.github.com>
2020-07-16 14:37:25 -0400

    Update BatteryDataProgram.m
    Updated to include command line statues updates, and doesn't try to plot cycles if you don't change the "Cycle 1", "Cycle 2", and "Cycle 3" boxes.

--- BatteryDataProgram.m
+++ BatteryDataProgram.m
@@ -29,26 +29,37 @@ h.c(9) = uicontrol('style','checkbox','units','pixels',...
 h.p = uicontrol('style','pushbutton','units','pixels',...
                 'position',[180,25,140,50],'string','Continue','FontSize',14,...
                 'callback',@p_call);
-            
+        
     % Pushbutton callback
     function p_call(varargin)
         vals = get(h.c,'Value');
         checked = find([vals{:}]);
         filename = h.c(1).String;
         current_density = h.c(2).String;
+        c1 = h.c(3).String;
+        c2 = h.c(4).String;
+        c3 = h.c(5).String;
         TT1 = filename == "Input Filepath";
         TT2 = current_density == "Input Current Density";
         if TT1+TT2>=1
             if filename == "Input Filepath"
-                'Please Enter File Path'
+                disp('Please Enter File Path')
             end
             if current_density == "Input Current Density"
-                'Please Enter Current Density'
+                disp('Please Enter Current Density')
             end
             return
-        else
+        elseif c1 == "Cycle 1" || c2 == "Cycle 2" || c3 == "Cycle 3"
+            disp("Proceeding without cycling plot")
+            disp('Getting data from spreadsheet')
             current_density_num = str2double(current_density);
-            cycles = [str2double(h.c(3).String), str2double(h.c(4).String), str2double(h.c(5).String)];
+            [a, b] = battery_data_read(filename);
+            avg_C = abs(mean(a(a(:,4)==4,7)));
+            m = avg_C./current_density_num;
+        else
+            disp('Getting data from spreadsheet and plotting cycles')
+            current_density_num = str2double(current_density);
+            cycles = [str2double(c1), str2double(c2), str2double(c3)];
             [a, b] = battery_data_read(filename);
             avg_C = abs(mean(a(a(:,4)==4,7)));
             m = avg_C./current_density_num;
@@ -66,15 +77,19 @@ h.p = uicontrol('style','pushbutton','units','pixels',...
         end
         
         if ismember(6,checked)
+            disp('Plotting battery statistics')
             battstat(b,m)
         end
         if ismember(7,checked)
+            disp('Plotting all cycles')
             cyclegroups(a,m)
         end
         if ismember(8,checked)
+            disp('Plotting dQ/dV')
             dqdvwithrests(a,m)
         end
         if ismember(9,checked)
+            disp('Plotting capacity retention')
             Capacity_Retention(b,m)
         end
         
