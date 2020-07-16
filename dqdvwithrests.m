cc90a233c1f166ab9f32f4a3d51613ed4b751990
LeoWGordon <53127431+LeoWGordon@users.noreply.github.com>
2020-07-16 14:38:57 -0400

    Update dqdvwithrests.m
    - Fixed issue of incorrect units.
    - Added y-axis limits based on the smallest/biggest values.

--- dqdvwithrests.m
+++ dqdvwithrests.m
@@ -1,5 +1,5 @@
 function dqdvwithrests(a,mass)
-
+% mass in g
     ccc = a(:,5); % cycle index column
 
     n = max(ccc); % number of cycles
@@ -14,6 +14,7 @@ function dqdvwithrests(a,mass)
     end
 
     cnt = 0;
+    ymaxall = nan(size(thresvec));
     % j = [1:1:n];
         for i = thresvec % Finds data for every 50th cycle, change 50 to change the step size
             cnt = cnt+1;
@@ -21,25 +22,25 @@ function dqdvwithrests(a,mass)
             si = cn(:,4); % finds the step indices
 
             if cn(si==2,7)<0
-                dischargecap = cn(si==2,9)*1000/mass;
+                dischargecap = cn(si==2,9)/mass;
                 dischargevoltage = cn(si==2,6);
-                chargecap = cn(si==4,8)*1000/mass;
+                chargecap = cn(si==4,8)/mass;
                 chargevoltage = cn(si==4,6);
 
-                drestcap = cn(si==3,9)*1000/mass;
+                drestcap = cn(si==3,9)/mass;
                 drestvolt = cn(si==3,6);
-                crestcap = cn(si==5,8)*1000/mass;
+                crestcap = cn(si==5,8)/mass;
                 crestvolt = cn(si==5,6);
 
             else
-                dischargecap = cn(si==4,9)*1000/mass;
+                dischargecap = cn(si==4,9)/mass;
                 dischargevoltage = cn(si==4,6);
-                chargecap = cn(si==2,8)*1000/mass;
+                chargecap = cn(si==2,8)/mass;
                 chargevoltage = cn(si==2,6);
 
-                drestcap = cn(si==5,9)*1000/mass;
+                drestcap = cn(si==5,9)/mass;
                 drestvolt = cn(si==5,6);
-                crestcap = cn(si==3,8)*1000/mass;
+                crestcap = cn(si==3,8)/mass;
                 crestvolt = cn(si==3,6);
 
             end
@@ -52,15 +53,15 @@ function dqdvwithrests(a,mass)
             % Savitzky-Golay filter to smooth the data
             % Increase 3rd entry to smooth more, or vice versa
             sgf0 = sgolayfilt(dydx,1,51); 
-
+            ymaxall(i) = max(sgf0);
+            yminall(i) = min(sgf0);
             % Plot figure
             txt = ['Cycle ',num2str(i)];
             c = jet(n);
 
             p1 = plot(volt,sgf0,'color',c(cnt*(floor(n/clevels)),:),'displayname',txt,'linewidth',2);
-    %         axis([0 2.2 -100 500]); % add to fix axes, noisy data shrinks the rest
             xlim([min(volt)-0.1 max(volt)+0.1])
-            ylim([min(sgf0)-250 max(sgf0)+250])
+            ylim([min(yminall)-50 max(ymaxall)+50])
             xlabel('Voltage (V)');
             ylabel('^{dQ}/_{dV} (mA h g^{-1} V^{-1})');
             hold on
