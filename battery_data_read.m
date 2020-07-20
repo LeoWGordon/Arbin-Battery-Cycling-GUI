34d3492b1905e1ea78b87fa96e7f4f1d085b28d6
LeoWGordon <53127431+LeoWGordon@users.noreply.github.com>
2020-07-20 15:23:22 -0400

    Update battery_data_read.m

--- battery_data_read.m
+++ battery_data_read.m
@@ -3,23 +3,23 @@
 
 function [a, b] = battery_data_read(filename)
 
-[xxx, sheets] = xlsfinfo(filename);
+sheets = sheetnames(filename);
 
 k=0;
 kk=0;
 count=0;
 while kk<1
     count=count+1;
-   if contains(sheets{count},'Channel') 
-        a = readmatrix(filename,'Sheet',sheets{count});
+   if contains(sheets(count),'Channel') 
+        a = readmatrix(filename,'Sheet',sheets(count));
         kk=1;
     end 
 end
 count=0;
 while k<1
     count=count+1;
-    if contains(sheets{count},'Statistics') 
-        b = readmatrix(filename,'Sheet',sheets{count});
+    if contains(sheets(count),'Statistics') 
+        b = readmatrix(filename,'Sheet',sheets(count));
         k=1;
     end
 end
