3f05c5a0ebe059a274d3826334af7fa5595c96b1
LeoWGordon <53127431+LeoWGordon@users.noreply.github.com>
2020-11-16 11:26:31 -0500

    Update BatteryDataProgram.m

--- BatteryDataProgram.m
+++ BatteryDataProgram.m
@@ -3,13 +3,16 @@
 % all cycles, dQ/dV, and capacity retention.
 % dQ/dV requires the use of signal processing toolbox.
 % Code prepared by Leo W. Gordon.
+% Head Programme - GUI to input data and choose which figures to get.
+% Gives figures with: 3 cycles of your choosing, battery statistics, 
+% all cycles, dQ/dV, and capacity retention.
 function BatteryDataProgram
 % Create figure
 h.f = figure('units','normalized','position',[0.35,0.25,0.3,0.5],...
              'toolbar','none','menu','none');
 % Create Text Input Fields
 h.c(1) = uicontrol('Style','edit','Units','normalized',...
-                'Position',[0.1,0.9,0.8,0.05],'String','Input Filepath','FontSize',14);
+                'Position',[0.3,0.9,0.6,0.05],'String','Input File Path','FontSize',14);
 h.c(2) = uicontrol('Style','edit','Units','normalized',...
                 'Position',[0.1,0.825,0.8,0.05],'String','Input Current Density (mA/g)','FontSize',14);
 h.c(3) = uicontrol('Style','edit','Units','normalized',...
@@ -28,9 +31,45 @@ h.c(8) = uicontrol('style','checkbox','units','normalized',...
 h.c(9) = uicontrol('style','checkbox','units','normalized',...
                 'position',[0.1,0.3,0.3,0.05],'string','Capacity Retention','FontSize',14); 
 % Create Continue Pushbutton   
-h.p = uicontrol('style','pushbutton','units','normalized',...
+h.p(1) = uicontrol('style','pushbutton','units','normalized',...
                 'position',[0.3,0.1,0.4,0.1],'string','Continue','FontSize',14,...
                 'callback',@p_call);
+h.p(2) = uicontrol('style','pushbutton','units','normalized',...
+                'position',[0.1,0.9,0.2,0.05],'string','Browse Data','FontSize',14,...
+                'callback',@p_browsefolder);
+            
+% This creates the 'background' axes
+ha = axes('units','normalized', ...
+            'position',[0.4 0.3 0.5 0.35]);
+% Move the background axes to the bottom
+uistack(ha,'bottom');
+% Load in a background image and display it using the correct colors
+% The image used below, is in the Image Processing Toolbox.  If you do not have %access to this toolbox, you can use another image file instead.
+try
+I=imread('/Users/leo/Documents/All_Figures/Tex_figures/Messinger_Logo_5.png');
+catch
+I=imread('/Users/lgordon/Documents/All_Figures/Tex_figures/Messinger_Logo_5.png');
+% I=imread('/Users/leo/Desktop/scihub.png');
+end
+hi = imagesc(I);
+% colormap gray
+% Turn the handlevisibility off so that we don't inadvertently plot into the axes again
+% Also, make the axes invisible
+set(ha,'handlevisibility','off', ...
+            'visible','off')  
+
+    function p_browsefolder(varargin)
+       
+        [name, path] = uigetfile('~/*.xlsx','Choose Cycling Spreadsheet');
+        if isequal(name,0)
+           disp('User selected Cancel');
+           set(h.c(1),'String','Input File Path')
+        else
+           disp(['User selected ', fullfile(name)]);
+           set(h.c(1),'String',fullfile(path,name))
+        end
+        
+    end
         
     % Pushbutton callback
     function p_call(varargin)
@@ -94,7 +133,7 @@ h.p = uicontrol('style','pushbutton','units','normalized',...
             disp('Plotting capacity retention')
             Capacity_Retention(b,m)
         end
-        
+        disp('Complete')
         closereq()
     end
 end
